0176a4fce3952c0bccfb145b513c720e50fcbea2
mysoyul <vega2k_@naver.com>
2024-04-22 17:30:10 +0900

    DatabaseRunner update

--- src/main/java/com/basic/myspringboot/runner/DatabaseRunner.java
+++ src/main/java/com/basic/myspringboot/runner/DatabaseRunner.java
@@ -1,5 +1,6 @@
 package com.basic.myspringboot.runner;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -8,19 +9,24 @@ import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 
 @Component
 @Order(1)
+@Slf4j
 public class DatabaseRunner implements ApplicationRunner {
     @Autowired
     DataSource dataSource;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("DataSource 구현객체는 ?? " + dataSource.getClass().getName());
+        System.out.println("DataSource 구현객체 클래스명 " + dataSource.getClass().getName());
         try (Connection connection = dataSource.getConnection()) {
-            System.out.println(connection.getMetaData().getURL());
-            System.out.println(connection.getMetaData().getUserName());
+            DatabaseMetaData metaData = connection.getMetaData();
+            log.info("Database Product Name = {}", metaData.getDatabaseProductName());
+            log.info("Database URL = {}", metaData.getURL());
+            log.info("Database Username = {}", metaData.getUserName());
+
         }
     }
 }
